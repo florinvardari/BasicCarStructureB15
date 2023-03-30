9698764499c7fecc3457a173e2227e3262d101d6
florinvardari <florin.vardari@gmail.com>
2023-03-29 20:13:49 -0400

    the engine has been added

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,11 @@ public class Main {
       audi.color="black";
       audi.model="A3";
       audi.power="5000cc";
+
+      Engine x=new Engine();
+      x.cylinder="8";
+      x.size="1234cc";
+      x.weight="6543k";
+
     }
 }
